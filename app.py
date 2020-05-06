a70aca3ca0242a96f3476e08a0d7c01c58419be4
margaret mwangi <margaret13mwangi@gmail.com>
2020-05-06 19:36:46 +0300

    new files

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
 from wtform_fields import *
+from models import *
+
 
 # Configure app
 app = Flask(__name__)
 app.secret_key ='later'
+
+
+# Configure database
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://slxchntbrzumdb:6b63f8c71961cbd533796cd453f9c722ff9a4ecd44ee3d724620d96495572a06@ec2-3-222-30-53.compute-1.amazonaws.com:5432/d73usrh8cslhie'
+db = SQLAlchemy(app)
+
 
 
 @app.route("/",methods=['GET','POST'])
@@ -12,7 +20,19 @@ def index():
 
     reg_form = RegistrationForm()
     if reg_form.validate_on_submit():
-        return  "am boo"
+        username =reg_form.username.data
+        password = reg_form.password.data
+
+        # Check username exist
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            return "name taken!"
+        
+        # Add user to DB
+        user = User(username=username,password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "good work"
 
     return render_template("index.html",form=reg_form)
 
