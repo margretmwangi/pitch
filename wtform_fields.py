03f650aed9edea13cfc6088780490ecd357dd043
margaret mwangi <margaret13mwangi@gmail.com>
2020-05-06 18:55:39 +0300

    modified files

--- wtform_fields.py
+++ wtform_fields.py
@@ -4,12 +4,7 @@ from wtforms.validators import InputRequired,Length,EqualTo
 
 from models import User
 
-def invalid_credentials(form ,field):
-    """ Username and password checker """
 
-    username_entered =form.username.data
-     password_entered = field.data
-    
  
    # check credentials  are valid 
 
@@ -29,20 +24,3 @@ class RegistrationForm(FlaskForm):
     Length(min=4,max=25, message="password must be  between 4 characters and 25 characters")])
     confirm_pswd = PasswordField('confirm_pswd_label', validators=[InputRequired(message="Password Required"),EqualTo("password",message="password must match")])
     submit_button = SubmitField('create')
-
-def validate_username(self,username):
-    user_object = User.query.filter_by(username=username.data).first()
-    if  user_object:
-        raise ValidationError('username  already taken.select a different user')
-
-    class LoginForm(FlaskForm):
-        """Login form """
-
-        username = StringField('username_label', validators=[InputRequired( message="Username required")])
-        password = StringField('password_label', validators=[InputRequired(message= "password required"), invalid_credentials])
-
-        submit_button = SubmitField('Login')
-
-
-
-        
